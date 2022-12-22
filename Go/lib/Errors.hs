e37cf1e71bf41c55a6cea8acb05befdec58d00a8
Azim Muradov <azim.muradov.dev@gmail.com>
2022-12-22 17:13:57 +0300

    Remove `unreachable`

--- Go/lib/Errors.hs
+++ Go/lib/Errors.hs
@@ -5,9 +5,3 @@ todo a = error ("Not implemented!" ++ " (" ++ show a ++ ")")
 
 todo' :: a
 todo' = error "Not implemented!"
-
-unreachable :: Show a1 => a1 -> a2
-unreachable a = error ("Unreachable!" ++ " (" ++ show a ++ ")")
-
-unreachable' :: a
-unreachable' = error "Unreachable!"
