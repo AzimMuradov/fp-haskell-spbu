c4a4640553d1bf138c402e1e55a4f154df24aafb
Azim Muradov <azim.muradov.dev@gmail.com>
2022-12-28 02:44:38 +0300

    Update `bool` test

--- Go/test-integration/tests/bool.go
+++ Go/test-integration/tests/bool.go
@@ -2,6 +2,8 @@ func main() {
   var a = false;
   var b = true;
 
+  println(!a);     // should print "true"
+  println(!b);     // should print "false"
   println(a || b); // should print "true"
   println(a && b); // should print "false"
   println(a == b); // should print "false"
