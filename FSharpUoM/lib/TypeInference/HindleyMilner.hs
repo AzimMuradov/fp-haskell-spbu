5a2bcafee52e4c65344f55fd88f78ce7dc912512
Zadorozhnyy Alexander <sasha.zadorojnii2013@yandex.ru>
2023-01-08 12:58:57 +0300

    Fixed some bugs with Units of Measure Inference.

--- FSharpUoM/lib/TypeInference/HindleyMilner.hs
+++ FSharpUoM/lib/TypeInference/HindleyMilner.hs
@@ -32,6 +32,7 @@ import Data.Text (pack)
 import GHC.Generics (Generic1)
 import Prelude hiding (lookup)
 
+type MeasureV = Map Identifier Integer
 
 data HType a
   = TyVarF Identifier
@@ -39,13 +40,8 @@ data HType a
   | TyIntF a
   | TyDoubleF a
   | TyFunF a a
-  | TyVarDeclF Identifier a
-  | TyFunDeclF Identifier a
   | MPureF
-  | TyMeasureF Identifier
-  | TyMulMeasureExprF a a
-  | TyDivMeasureExprF a a
-  | TyExpMeasureExprF a a
+  | TyMeasureF MeasureV
   deriving (Show, Eq, Functor, Foldable, Traversable, Generic1, Unifiable)
 
 type TypeF = Fix HType
@@ -62,8 +58,8 @@ type UPolytype = Poly UType
 pattern TyVar :: Identifier -> TypeF
 pattern TyVar v = Fix (TyVarF v)
 
-pattern TyMeasure :: Identifier -> TypeF
-pattern TyMeasure  m = Fix (TyMeasureF m)
+pattern TyMeasure :: MeasureV -> TypeF
+pattern TyMeasure m = Fix (TyMeasureF m)
 
 pattern TyInt :: TypeF -> TypeF
 pattern TyInt x = Fix (TyIntF x)
@@ -80,25 +76,10 @@ pattern TyBool = Fix TyBoolF
 pattern TyFun :: TypeF -> TypeF -> TypeF
 pattern TyFun t1 t2 = Fix (TyFunF t1 t2)
 
-pattern TyVarDecl :: Identifier -> TypeF -> TypeF
-pattern TyVarDecl x t = Fix (TyVarDeclF x t)
-
-pattern TyFunDecl :: Identifier -> TypeF -> TypeF
-pattern TyFunDecl x t = Fix (TyFunDeclF x t)
-
-pattern TyMulMeasureExpr :: TypeF -> TypeF -> TypeF
-pattern TyMulMeasureExpr t1 t2 = Fix (TyMulMeasureExprF t1 t2)
-
-pattern TyDivMeasureExpr :: TypeF -> TypeF -> TypeF
-pattern TyDivMeasureExpr t1 t2 = Fix (TyDivMeasureExprF t1 t2)
-
-pattern TyExpMeasureExpr :: TypeF -> TypeF -> TypeF
-pattern TyExpMeasureExpr t1 t2 = Fix (TyExpMeasureExprF t1 t2)
-
 pattern UTyVar :: Identifier -> UType
 pattern UTyVar v = UTerm (TyVarF v)
 
-pattern UTyMeasure :: Identifier -> UType
+pattern UTyMeasure :: MeasureV -> UType
 pattern UTyMeasure m = UTerm (TyMeasureF m)
 
 pattern UTyInt :: UType -> UType
@@ -116,56 +97,57 @@ pattern UTyBool = UTerm TyBoolF
 pattern UTyFun :: UType -> UType -> UType
 pattern UTyFun t1 t2 = UTerm (TyFunF t1 t2)
 
-pattern UTyVarDecl :: Identifier -> UType -> UType
-pattern UTyVarDecl x t = UTerm (TyVarDeclF x t)
-
-pattern UTyFunDecl :: Identifier -> UType -> UType
-pattern UTyFunDecl x t = UTerm (TyFunDeclF x t)
-
-pattern UTyMulMeasureExpr :: UType -> UType -> UType
-pattern UTyMulMeasureExpr m1 m2 = UTerm (TyMulMeasureExprF m1 m2)
-
-pattern UTyDivMeasureExpr :: UType -> UType -> UType
-pattern UTyDivMeasureExpr m1 m2 = UTerm (TyDivMeasureExprF m1 m2)
-
-pattern UTyExpMeasureExpr :: UType -> UType -> UType
-pattern UTyExpMeasureExpr m1 m2 = UTerm (TyExpMeasureExprF m1 m2)
-
 toTypeF :: Type -> TypeF
 toTypeF x = case x of
   TBool -> Fix TyBoolF
-  (TInt measure) -> case measure of 
+  (TInt measure) -> case measure of
     Just m -> Fix (TyIntF $ fromMToF m)
     Nothing -> Fix (TyIntF MPure)
-  (TDouble measure) -> case measure of 
+  (TDouble measure) -> case measure of
     Just m -> Fix (TyDoubleF $ fromMToF m)
     Nothing -> Fix (TyDoubleF MPure)
   (TFun t1 t2) -> Fix $ TyFunF (toTypeF t1) (toTypeF t2)
 
 fromMToF :: MeasureTypeExpr -> TypeF
-fromMToF x = case x of
-   (MIdentifier ident) -> TyMeasure ident
-   (MTypesMul e1 e2) -> TyMulMeasureExpr (fromMToF e1) (fromMToF e2)
-   (MTypesDiv e1 e2) -> TyDivMeasureExpr (fromMToF e1) (fromMToF e2)
-   (MTypesExp e1 _) -> TyExpMeasureExpr (fromMToF e1) (Fix $ TyIntF MPure)
+fromMToF = TyMeasure . convertMeasure
+
+convertMeasure :: MeasureTypeExpr -> MeasureV
+convertMeasure =
+  M.filter (/= 0) . \case
+    (MIdentifier ident) -> M.singleton ident 1
+    (MTypesMul e1 e2) -> (sumMaps (convertMeasure e1) (convertMeasure e2))
+    (MTypesExp e1 c) -> (* c) <$> convertMeasure e1
+    (MTypesDiv e1 e2) -> (sumMaps (convertMeasure e1) ((* (-1)) <$> convertMeasure e2))
+
+sumMaps :: (Ord k, Num a) => Map k a -> Map k a -> Map k a
+sumMaps a b =
+  let partA = M.union a b
+      partB = M.intersection b a
+   in M.foldlWithKey (\acc k v -> M.insert k (acc M.! k + v) acc) partA partB
+
+opWithMeasure :: UType -> UType
+opWithMeasure = undefined
 
 fromMToUM :: MeasureTypeExpr -> UType
-fromMToUM x = case x of
-   (MIdentifier ident) -> UTyMeasure ident
-   (MTypesMul e1 e2) -> UTyMulMeasureExpr (fromMToUM e1) (fromMToUM e2)
-   (MTypesDiv e1 e2) -> UTyDivMeasureExpr (fromMToUM e1) (fromMToUM e2)
-   (MTypesExp e1 _) -> UTyExpMeasureExpr (fromMToUM e1) (UTerm $ TyIntF UMPure)
+fromMToUM = UTyMeasure . convertMeasure
 
+mulM :: UType -> UType -> Infer UType
+mulM (UTyMeasure m1) (UTyMeasure m2) = return $ UTyMeasure $ M.filter (/= 0) $ sumMaps m1 m2
+mulM _ _ = throwError Unreachable
+
+divM :: UType -> UType -> Infer UType
+divM (UTyMeasure m1) (UTyMeasure m2) = return $ UTyMeasure $ M.filter (/= 0) $ sumMaps m1 ((* (-1)) <$> m2)
+divM _ _ = throwError Unreachable
 
 fromTypeToUType :: Type -> UType
 fromTypeToUType x = case x of
   TBool -> UTerm TyBoolF
   (TInt measure) -> case measure of
     Just m -> UTerm (TyIntF $ fromMToUM m)
-    Nothing -> UTerm (TyIntF UMPure)
+    Nothing -> UTerm (TyIntF $ UTyMeasure M.empty)
   (TDouble measure) -> case measure of
     Just m -> UTerm (TyDoubleF $ fromMToUM m)
-    Nothing -> UTerm (TyDoubleF UMPure)
+    Nothing -> UTerm (TyDoubleF $ UTyMeasure M.empty)
   (TFun t1 t2) -> UTerm $ TyFunF (fromTypeToUType t1) (fromTypeToUType t2)
 
 type Infer = ReaderT Ctx (ExceptT TypeError (IntBindingT HType Identity))
@@ -178,26 +160,22 @@ lookup x = do
   case x of
     (Var v) -> maybe (throwError $ UnboundVar v) instantiate (M.lookup v ctx)
     (Measure m) -> maybe (throwError $ UnboundMeasure m) instantiate (M.lookup m ctx)
-  
+
 checkForDuplicate :: LookUpType -> Infer UType
 checkForDuplicate x = do
   ctx <- ask
   case x of
-    (Var v) -> 
+    (Var v) ->
       case M.lookup v ctx of
         (Just _) -> throwError $ DuplicateDifinition v
         Nothing -> return $ UTyVar v
-    (Measure m) -> 
+    (Measure m) ->
       case M.lookup m ctx of
         (Just _) -> throwError $ DuplicateMeasureDifinition m
         Nothing -> return $ UTyVar m
 
 withBinding :: MonadReader Ctx m => Identifier -> UPolytype -> m a -> m a
 withBinding x ty = local (M.insert x ty)
-
--- bindHelper :: MonadReader Ctx m => (m a -> m a) -> [Identifier] -> UPolytype -> m a -> m a
--- bindHelper state [] _ = state
--- bindHelper _ (x : xs) ty = bindHelper (local (M.insert x ty)) xs ty
 
 ucata :: Functor t => (v -> a) -> (t a -> a) -> UTerm t v -> a
 ucata f _ (UVar v) = f v
@@ -224,8 +202,9 @@ instance FreeVars UPolytype where
 instance FreeVars Ctx where
   freeVars = fmap S.unions . mapM freeVars . M.elems
 
-data LookUpType = Var Identifier
-                | Measure Identifier
+data LookUpType
+  = Var Identifier
+  | Measure Identifier
 
 data TypeError where
   EmptyList :: TypeError
@@ -234,7 +213,9 @@ data TypeError where
   DuplicateMeasureDifinition :: Identifier -> TypeError
   UnboundVar :: Identifier -> TypeError
   UnboundMeasure :: Identifier -> TypeError
+  MeasureMismatch :: MeasureV -> MeasureV -> TypeError
   Infinite :: IntVar -> UType -> TypeError
+  ImpossibleOpApplication :: UType -> UType -> TypeError
   Mismatch :: HType UType -> HType UType -> TypeError
 
 instance Fallible HType IntVar TypeError where
@@ -245,9 +226,6 @@ fresh :: Infer UType
 fresh = UVar <$> lift (lift freeVar)
 
 (=:=) :: UType -> UType -> Infer UType
-(UTyFunDecl _ t) =:= (UTyFunDecl _ t') = t =:= t'
-(UTyFunDecl _ t) =:= t' = lift $ t U.=:= t'
-t' =:= (UTyFunDecl _ t) = lift $ t U.=:= t'
 s =:= t = lift $ s U.=:= t
 
 applyBindings :: UType -> Infer UType
--- FSharpUoM/lib/TypeInference/TIRealization.hs
+++ FSharpUoM/lib/TypeInference/TIRealization.hs
@@ -10,10 +10,10 @@ module TypeInference.TIRealization where
 
 import Ast
 import Control.Monad.Except
+import qualified Data.Map as M
 import Data.Maybe
 import TypeInference.HindleyMilner
 import Prelude hiding (lookup)
-import Control.Monad.Reader (ask)
 
 check :: Expr -> UType -> Infer UType
 check e ty = do
@@ -25,112 +25,52 @@ checkStatement st ty = do
   ty' <- inferStatement st
   ty =:= ty'
 
-binOpInfer :: Expr -> Expr -> Infer UType
-binOpInfer e1 e2 = do
-  t <- inferSingle e1
-  case t of
-    (UTyInt m) -> do
-      _ <- check e2 (UTyInt m)
-      return (UTyInt m)
-    (UTyDouble m) -> do
-      _ <- check e2 (UTyDouble m)
-      return (UTyDouble m)
-    _ -> do
-      check e2 t
-
-booleanOpInfer :: Expr -> Expr -> Infer UType
-booleanOpInfer e1 e2 = do
-  t <- inferSingle e1
-  case t of
-    (UTyInt m) -> do
-      _ <- check e2 (UTyInt m)
-      return UTyBool
-    (UTyDouble m) -> do
-      _ <- check e2 (UTyDouble m)
-      return UTyBool
-    _ -> do
-      check e2 t
-
 helpInferStatements :: [Statement] -> Infer UType -> Infer UType
 helpInferStatements [] pr = pr
-helpInferStatements ((SVarDecl (VarDecl (ident, t) st)) : xs) _ = do
-  res <- inferBlock st -- Dup
+helpInferStatements ((SVarDecl (VarDecl (ident, t) body)) : xs) _ = do
   _ <- checkForDuplicate (Var ident)
-  case t of
-    Just ty -> do
-      let types = fromTypeToUType ty
-      r <- res =:= types
-      withBinding ident (Forall [] r) (return r)
-    Nothing -> withBinding ident (Forall [] res) (helpInferStatements xs $ return res)
-helpInferStatements ((SFunDecl (FunDecl ident val)) : xs) _ = do
-  -- TODO : mb chanhe smt
-  res <- inferSingle (EValue (VFun val))
-  tp' <- do
-    _ <- checkForDuplicate (Var ident)
-    return $ UTyFunDecl ident res
-  withBinding ident (Forall [] tp') (helpInferStatements xs $ return res)
-helpInferStatements ((SRecFunDecl (RecFunDecl ident val)) : xs) _ = do
+  res <- inferBlock body
+  vType <- maybe (return res) ((=:=) res <$> fromTypeToUType) t
+  pvType <- generalize vType
+  withBinding ident pvType (helpInferStatements xs $ return vType)
+helpInferStatements ((SFunDecl (FunDecl ident (Fun args body))) : xs) _ = do
+  _ <- checkForDuplicate (Var ident)
+  res <- inferFun args body
+  withBinding ident (Forall [] res) (helpInferStatements xs $ return res)
+helpInferStatements ((SRecFunDecl (RecFunDecl ident (Fun args body))) : xs) _ = do
+  _ <- checkForDuplicate (Var ident)
   preT <- fresh
-  next <- do
-    _ <- checkForDuplicate (Var ident)
-    withBinding ident (Forall [] preT) $ inferSingle (EValue (VFun val))
+  next <- withBinding ident (Forall [] preT) $ inferFun args body
   withBinding ident (Forall [] next) (helpInferStatements xs $ return next)
--- return $ UTyFunDecl ident next
--- tp' <- tp
--- withBinding ident (Forall [] res) (helpInferStatements xs $ return res)
--- where
---   tp = do
---     st' <- infer (EValue val)
---     UTyFunDecl ident <$> (return st')
 helpInferStatements ((SMeasureDecl (MeasureDecl ident mexpr)) : xs) _ = do
   _ <- checkForDuplicate (Measure ident)
   t <- case mexpr of
     Just m -> inferMeasure m
-    Nothing -> return $ UTyMeasure ident
+    Nothing -> return $ UTyMeasure $ M.singleton ident 1
   withBinding ident (Forall [] t) (helpInferStatements xs (return t))
-
--- let res = inferMeasure mexpr -- Dup
--- tp' <- tp
--- withBinding ident (Forall [] tp') (helpInferStatements xs res)
--- where
---   tp = do
---     _ <- checkForDuplicate ident
---     st' <- helpInferStatements st pr
---     case t of
---       Just ty -> do
---         let types = fromTypeToUType ty
---         UTyMeasure ident <$> st' =:= types
---       Nothing -> return $ UTyMeasure ident st'
-
 helpInferStatements ((SExpr e) : xs) _ = do
   res <- inferSingle e
   helpInferStatements xs (return res)
 
--- topLevelInference :: [Statement] -> [Infer UType]
--- topLevelInference [] = throwError EmptyList : []
--- topLevelInference (x : xs) = inferStatement ((: []) x) : topLevelInference xs
-
 inferStatement :: [Statement] -> Infer UType
-inferStatement x = helpInferStatements x (throwError EmptyList)
-
-inferMeasure' :: Maybe MeasureTypeExpr -> Infer UType
-inferMeasure' = maybe (return UMPure) inferMeasure
+inferStatement x = do
+  helpInferStatements x (throwError EmptyList)
 
 inferMeasure :: MeasureTypeExpr -> Infer UType
 inferMeasure (MIdentifier m) = lookup (Measure m)
-inferMeasure (MTypesMul m1 m2) = do
-  m1' <- inferMeasure m1
-  m2' <- inferMeasure m2
-  return $ UTyMulMeasureExpr m1' m2'
-inferMeasure (MTypesDiv m1 m2) = do
-  m1' <- inferMeasure m1
-  m2' <- inferMeasure m2
-  return $ UTyMulMeasureExpr m1' m2'
-inferMeasure (MTypesExp m1 _) = do
-  m1' <- inferMeasure m1
-  return $ UTyMulMeasureExpr m1' (UTyInt UMPure)
+inferMeasure mul@(MTypesMul m1 m2) = do
+  _ <- inferMeasure m1
+  _ <- inferMeasure m2
+  return $ fromMToUM mul
+inferMeasure d@(MTypesDiv m1 m2) = do
+  _ <- inferMeasure m1
+  _ <- inferMeasure m2
+  return $ fromMToUM d
+inferMeasure e@(MTypesExp m1 _) = do
+  _ <- inferMeasure m1
+  return $ fromMToUM e
 
-inferBlock :: [Expr] -> Infer UType -- TODO : mb wrong
+inferBlock :: [Expr] -> Infer UType
 inferBlock [] = throwError EmptyList
 inferBlock [x] = inferSingle x
 inferBlock (x : xs) = inferSingle x >> inferBlock xs
@@ -138,22 +78,17 @@ inferBlock (x : xs) = inferSingle x >> inferBlock xs
 inferSingle :: Expr -> Infer UType
 inferSingle (EIdentifier x) = lookup (Var x)
 inferSingle (EValue (VBool _)) = return UTyBool
-inferSingle (EValue (VInt _ m)) = do
-  res <- inferMeasure' m
+inferSingle (EValue (VInt _ measure)) = do
+  res <- case measure of
+    Just m -> inferMeasure m
+    Nothing -> return $ UTyMeasure M.empty
   return $ UTyInt res
-inferSingle (EValue (VDouble _ m)) = do
-  res <- inferMeasure' m
+inferSingle (EValue (VDouble _ measure)) = do
+  res <- case measure of
+    Just m -> inferMeasure m
+    Nothing -> return $ UTyMeasure M.empty
   return $ UTyDouble res
-inferSingle (EValue (VFun (Fun xs body))) = infer' xs body
-  where
-    infer' args st = case args of
-      [] -> inferBlock body
-      ((ident, Just t) : ys) ->
-        let ut = fromTypeToUType t
-         in withBinding ident (Forall [] ut) $ UTyFun ut <$> infer' ys st
-      ((ident, Nothing) : ys) -> do
-        argTy <- fresh
-        withBinding ident (Forall [] argTy) $ UTyFun argTy <$> infer' ys st
+inferSingle (EValue (VFun (Fun xs body))) = inferFun xs body
 inferSingle (EIf e1 e2 e3) = do
   _ <- check e1 UTyBool
   e2' <- inferBlock e2
@@ -164,7 +99,58 @@ inferSingle (EOperations (NotOp x)) = do
   return UTyBool
 inferSingle (EOperations (BooleanOp x)) = booleanOpInfer (bL x) (bR x)
 inferSingle (EOperations (ComparisonOp x)) = booleanOpInfer (cL x) (cR x)
-inferSingle (EOperations (ArithmeticOp x)) = binOpInfer (aL x) (aR x)
+inferSingle (EOperations (ArithmeticOp x)) =
+  case x of
+    (PlusOp l r) -> simpleArithmeticOpInfer l r
+    (MinusOp l r) -> simpleArithmeticOpInfer l r
+    (ModOp l r) -> simpleArithmeticOpInfer l r
+    (MulOp l r) -> do
+      t1 <- inferSingle l
+      t2 <- inferSingle r
+      case (t1, t2) of
+        (UTyInt m1, UTyInt m2) -> do
+          t <- mulM m1 m2
+          return $ UTyInt t
+        (UTyDouble m1, UTyDouble m2) -> do
+          t <- mulM m1 m2
+          return $ UTyDouble t
+        (UTyInt _, _) -> t1 =:= t2
+        (_, UTyInt _) -> t1 =:= t2
+        (UTyDouble _, _) -> t1 =:= t2
+        (_, UTyDouble _) -> t1 =:= t2
+        _ -> throwError $ ImpossibleOpApplication t1 t2
+    (DivOp l r) -> do
+      t1 <- inferSingle l
+      t2 <- inferSingle r
+      case (t1, t2) of
+        (UTyInt m1, UTyInt m2) -> do
+          t <- divM m1 m2
+          return $ UTyInt t
+        (UTyDouble m1, UTyDouble m2) -> do
+          t <- divM m1 m2
+          return $ UTyDouble t
+        (UTyInt _, _) -> t1 =:= t2
+        (_, UTyInt _) -> t1 =:= t2
+        (UTyDouble _, _) -> t1 =:= t2
+        (_, UTyDouble _) -> t1 =:= t2
+        _ -> throwError $ ImpossibleOpApplication t1 t2
+    (ExpOp l r) -> do
+      t1 <- inferSingle l
+      t2 <- inferSingle r
+      case (t1, t2) of
+        (UTyDouble (UTyMeasure m1), UTyDouble (UTyMeasure m2)) ->
+          if null m1 && null m2
+            then return $ UTyDouble $ UTyMeasure m1
+            else throwError $ ImpossibleOpApplication t1 t2
+        (UTyDouble (UTyMeasure m1), _) ->
+          if null m1
+            then t1 =:= t2
+            else throwError $ ImpossibleOpApplication t1 t2
+        (_, UTyDouble (UTyMeasure m1)) ->
+          if null m1
+            then t1 =:= t2
+            else throwError $ ImpossibleOpApplication t1 t2
+        _ -> throwError $ ImpossibleOpApplication t1 t2
 inferSingle (ELetInV (x, Just pty) xdef body) = do
   let upty = toUPolytype (Forall [] $ toTypeF pty)
   upty' <- skolemize upty
@@ -175,26 +161,50 @@ inferSingle (ELetInV (x, Nothing) xdef body) = do
   ty <- inferBlock xdef
   pty <- generalize ty
   withBinding x pty $ inferBlock body
-
--- inferSingle (ELetInF x xdef body) = do
---   let upty = toUPolytype (Forall [] $ toTypeF pty)
---   upty' <- skolemize upty
---   bl <- inferBlock xdef
---   _ <- bl =:= upty'
---   withBinding x upty $ inferBlock body
-
--- inferSingle (ELetInF x xdef body) = do
---   ty <- inferBlock xdef
---   pty <- generalize ty
---   withBinding x pty $ inferBlock body
-
--- TODO : Fix bug with application
+inferSingle (ELetInF f (Fun args fbody) lbody) = do
+  fdef <- inferFun args fbody
+  pfdef <- generalize fdef
+  withBinding f pfdef $ inferBlock lbody
 inferSingle (EApplication e1 e2) = do
   funTy <- inferSingle e1
   argTy <- inferSingle e2
   resTy <- fresh
-  kek <- funTy =:= UTyFun argTy resTy
-  ctx <- ask
-  -- error $ show ctx <> show funTy <> " --- "  <> show argTy <> " --- "  <> show resTy <> " --- "  <> show kek
+  _ <- funTy =:= UTyFun argTy resTy
   return resTy
-inferSingle _ = undefined
+
+simpleArithmeticOpInfer :: Expr -> Expr -> Infer UType
+simpleArithmeticOpInfer e1 e2 = do
+  t1 <- inferSingle e1
+  t2 <- inferSingle e2
+  case (t1, t2) of
+    (UTyInt _, _) -> t1 =:= t2
+    (_, UTyInt _) -> t1 =:= t2
+    (UTyDouble _, _) -> t1 =:= t2
+    (_, UTyDouble _) -> t1 =:= t2
+    _ -> t1 =:= t2
+
+booleanOpInfer :: Expr -> Expr -> Infer UType
+booleanOpInfer e1 e2 = do
+  t1 <- inferSingle e1
+  t2 <- inferSingle e2
+  case (t1, t2) of
+    (UTyInt _, _) -> booleanOpHelper t1 t2
+    (_, UTyInt _) -> booleanOpHelper t1 t2
+    (UTyDouble _, _) -> booleanOpHelper t1 t2
+    (_, UTyDouble _) -> booleanOpHelper t1 t2
+    _ -> throwError $ ImpossibleOpApplication t1 t2
+
+booleanOpHelper :: UType -> UType -> Infer UType
+booleanOpHelper t1 t2 = do
+  _ <- t1 =:= t2
+  return UTyBool
+
+inferFun :: [(Identifier, Maybe Type)] -> [Expr] -> Infer UType
+inferFun args body = case args of
+  [] -> inferBlock body
+  ((ident, Just t) : ys) ->
+    let ut = fromTypeToUType t
+     in withBinding ident (Forall [] ut) $ UTyFun ut <$> inferFun ys body
+  ((ident, Nothing) : ys) -> do
+    argTy <- fresh
+    withBinding ident (Forall [] argTy) $ UTyFun argTy <$> inferFun ys body
